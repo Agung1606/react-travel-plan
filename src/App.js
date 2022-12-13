48210507630b6cfbbc8b52543811d16e9e86cd6f
Agung1606 <107747646+Agung1606@users.noreply.github.com>
2022-12-13 08:39:02 +0700

    adding navbar

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./style.css";
 
 import data from './data';
 
+import Navbar from './Navbar';
 import Card from './Card';
 
 export default function App() {
   const [dataTravel, setDataTravel] = useState(data);
 
   const cardElements = dataTravel.map(data => {
-    return <Card data={data} />
+    return <Card key={data.id} data={data} />
   })
 
   return (
     <div>
+      <Navbar />
       {cardElements}
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+export default function Navbar() {
+  return(
+    <nav>
+      <div>Travel plans</div>
+    </nav>
+  );
+}
